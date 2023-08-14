ac7f62166de51d96948d9e73e39d189ab763c3e9
catranch <catranch@users.noreply.github.com>
2023-08-14 14:29:36 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,54 @@
+from uuid import uuid4
+from fastapi import FastAPI, HTTPException 
+import os
+import json
+from pydantic import BaseModel
+from typing import Optional, Literal
+from fastapi.encoders import jsonable_encoder
+
+app = FastAPI()
+
+class ITEM(BaseModel):
+	name: str
+	price: float
+	genre: Literal["house","garage"]
+	item_id: Optional[str] = uuid4().hex
+
+ITEMS_FILE = "items.json"
+ROOMS_FILE = "rooms.json"
+ITEM_DATABASE = []
+
+#if doesn't exist then create it
+if os.path.exists(ITEMS_FILE):
+	with open(ITEMS_FILE,"r") as f:
+		ITEM_DATABASE = json.load(f)
+
+@app.get("/")
+def root():
+    return {"Message": "Ikea Inventory World"}
+
+@app.get("/list-items")
+async def list_items():
+	return {"books": ITEM_DATABASE}
+
+@app.get("/book-by-index/{index}")
+async def book_by_index(index: int):
+	if index < 0 or index >= len(ITEM_DATABASE):
+		raise HTTPException(404, f"Index {index} not working")
+	else:
+		return {"book": ITEM_DATABASE[index]}
+
+@app.post("/add-item")
+async def add_item(book: ITEM):
+	book.item_id = uuid4().hex
+	json_book = jsonable_encoder(book)
+	ITEM_DATABASE.append(json_book)
+	with open(ITEMS_FILE,"w") as f:
+		json.dump(ITEM_DATABASE, f)
+	return {"message": f"Item {book} added"}
+
+@app.post("/new-name")
+async def new_name():
+	return "new name"
+
+
